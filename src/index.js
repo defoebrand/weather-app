45a0716737b6bc1bfacabd6c12bcdbd980d7e04d
defoebrand <defoe.brand@gmail.com>
2020-11-18 17:44:12 +0200

    Add simple styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import headerBackground from './assets/atmosphere-earth.jpeg';
 import './style.scss';
 import contentCreator from './helpers/contentCreator'
-import {clearContent} from './helpers/helpers'
+import {clearContent, capFirst} from './helpers/helpers'
 
 const key = 'c718936db7e8ffb4daa086761e71f389'
 
 const body = document.querySelector('body')
+
+const header = contentCreator.withText('img', '', 'header')
+header.style.backgroundImage = headerBackground
+header.src = headerBackground
+// header.style.backgroundImage = "url('./assets/atmosphere-earth.jpeg')"
+body.appendChild(header)
+
 
 const form = contentCreator.withText('form')
 
@@ -17,7 +25,7 @@ const submitBtn = contentCreator.withValue('input', 'submit', "Check Weather")
 submitBtn.onclick = (e) => {
   e.preventDefault()
   clearContent(weatherBox)
-  getWeather(cityInput.value, key)
+  getWeather(cityInput.value, key, weatherBox)
   cityInput.value = ''
 }
 form.appendChild(submitBtn)
@@ -26,32 +34,31 @@ body.appendChild(form)
 
 body.appendChild(weatherBox);
 
-const getWeather = async (cityName, key) => {
-  const weatherBox = document.querySelector('.weatherBox')
+const getWeather = async (cityName, key, weatherBox) => {
  try {
    const weatherResult = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`, { mode: 'cors'});
    const weatherJson = await weatherResult.json()
      console.log(weatherJson)
 //somefunction
-        const name = contentCreator.withText('p', `${weatherJson.name}`)
+        const name = contentCreator.withText('p', `${weatherJson.name}`, 'name')
         weatherBox.appendChild(name)
-        const country = contentCreator.withText('p', `${weatherJson.sys.country}`)
+        const country = contentCreator.withText('p', `${weatherJson.sys.country}`, 'country')
         weatherBox.appendChild(country)
-        // const dateTime = contentCreator.withText('p', `${weatherJson.dt}`)
+        // const dateTime = contentCreator.withText('p', `${weatherJson.dt}`, 'dateTime')
         // weatherBox.appendChild(dateTime)
-        const feels_like = contentCreator.withText('p', `Feels like ${weatherJson.main.feels_like}deg C`)
+        const feels_like = contentCreator.withText('p', `Feels like ${weatherJson.main.feels_like}deg C`, 'feels_like')
         weatherBox.appendChild(feels_like)
-        const actual = contentCreator.withText('p', `Actually ${weatherJson.main.temp}deg C`)
+        const actual = contentCreator.withText('p', `Actual Temp ${weatherJson.main.temp}deg C`, 'actual_temp')
         weatherBox.appendChild(actual)
-        const humidity = contentCreator.withText('p', `${weatherJson.main.humidity}% Humidity`)
-        weatherBox.appendChild(humidity)
-        const clouds = contentCreator.withText('p', `${weatherJson.clouds.all}% Cloudy`)
+        // const humidity = contentCreator.withText('p', `${weatherJson.main.humidity}% Humidity`, 'humidity')
+        // weatherBox.appendChild(humidity)
+        const clouds = contentCreator.withText('p', `${weatherJson.clouds.all}% Cloudy`, 'cloudy')
         weatherBox.appendChild(clouds)
-        const description = contentCreator.withText('p', `${weatherJson.weather[0].description}`)
+        const description = contentCreator.withText('p', `${capFirst(weatherJson.weather[0].description)}`, 'description')
         weatherBox.appendChild(description)
-        const windSpeed = contentCreator.withText('p', `${weatherJson.wind.speed}kph?`)
+        const windSpeed = contentCreator.withText('p', `${weatherJson.wind.speed}kph?`, 'windSpeed')
         weatherBox.appendChild(windSpeed)
-        const windDeg = contentCreator.withText('p', `${weatherJson.wind.deg}deg`)
+        const windDeg = contentCreator.withText('p', `${weatherJson.wind.deg}deg`, 'windDeg')
         weatherBox.appendChild(windDeg)
 //endfunction
  } catch(err) {
@@ -59,3 +66,5 @@ const getWeather = async (cityName, key) => {
     }
   // return weatherBox
 }
+
+getWeather('vilnius', key, weatherBox)
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,19 @@ module.exports = {
         test: /\.s?[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader",],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
     ],
   },
 };
