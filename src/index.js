1bc776e9df61ac9b826235b24509145665c22065
defoebrand <defoe.brand@gmail.com>
2020-11-18 16:22:51 +0200

    Add function to retrieve data from the API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.scss';
 import contentCreator from './helpers/contentCreator'
 
+const key = 'c718936db7e8ffb4daa086761e71f389'
+
 const body = document.querySelector('body')
 
 const form = contentCreator.withText('form')
@@ -11,9 +13,24 @@ form.appendChild(cityInput)
 const submitBtn = contentCreator.withValue('input', 'submit', "Check Weather")
 submitBtn.onclick = (e) => {
   e.preventDefault()
-  alert(cityInput.value)
+  getWeather(cityInput.value, key)
   cityInput.value = ''
 }
 form.appendChild(submitBtn)
 
 body.appendChild(form)
+
+const getWeather = async (cityName, key) => {
+ const weatherBox = contentCreator.withText('p');
+ try {
+   const weatherResult = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`, { mode: 'cors'});
+   const weatherJson = await weatherResult.json()
+
+         weatherBox.innerHTML = weatherJson.main.feels_like
+         console.log(weatherJson)
+         document.body.appendChild(weatherBox)
+
+ } catch(err) {
+      console.log(err)
+    }
+}
