975e8ce1cb40dbf9d2b963fe62a4a9e06f99c586
defoebrand <defoe.brand@gmail.com>
2020-11-18 15:43:18 +0200

    Create basic form and center body

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.scss';
+
+const body = document.querySelector('body')
+
+const form = document.createElement('form')
+
+const cityInput = document.createElement('input')
+cityInput.type = 'text'
+cityInput.placeholder = 'Enter City Name...'
+form.appendChild(cityInput)
+
+const submitBtn = document.createElement('input')
+submitBtn.type = 'submit'
+submitBtn.value = "Check Weather"
+submitBtn.onclick = (e) => {
+  e.preventDefault()
+  alert(cityInput.value)
+}
+form.appendChild(submitBtn)
+
+body.appendChild(form)
